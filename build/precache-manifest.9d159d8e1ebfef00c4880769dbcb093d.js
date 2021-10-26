8fdc36e9b99ca56d6236e057352dc5c4a0d1f47e
Praneesha Rathnamalala <56305868+Praneesha-Irugal@users.noreply.github.com>
2021-10-26 12:46:02 +0530

    Add files via upload

---
+++ build/precache-manifest.9d159d8e1ebfef00c4880769dbcb093d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d036513496c69f7b873ac5d36636d20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa84a70d3c17b35797c8",
+    "url": "/static/css/main.5892d5a0.chunk.css"
+  },
+  {
+    "revision": "4a1f2ceb231af4392ea2",
+    "url": "/static/js/2.1d4b2518.chunk.js"
+  },
+  {
+    "revision": "927d15cad02b8fa4f913e8e01503c169",
+    "url": "/static/js/2.1d4b2518.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa84a70d3c17b35797c8",
+    "url": "/static/js/main.b32bc9d0.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  }
+]);
